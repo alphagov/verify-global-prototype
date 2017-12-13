0b3f27e07992aa3941a1f3ed173f302f0e68c0bc
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2017-12-13 10:50:12 +0000

    Added gov ie directory for all gov ie pages

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,10 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.get('/gov-ie/where-do-you-live', function (req, res) {
+  res.render('gov_ie/where_do_you_live')
+})
+
 // add your routes here
 
 module.exports = router

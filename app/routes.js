08c9ef5067c9cbec92c05b7cffbc3d28df882bcc
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2017-12-13 12:40:36 +0000

    Included public folder so we can check in static assets such as css and fonts

--- app/routes.js
+++ app/routes.js
@@ -1,22 +1,22 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
 // Route index page
 router.get('/', function (req, res) {
   res.render('index')
-})
+});
 
 router.get('/gov-ie/where-do-you-live', function (req, res) {
   res.render('gov_ie/where_do_you_live')
-})
+});
 
 router.get('/gov-uk/guidence/living-in-ireland', function (req, res) {
   res.render('gov_uk/guidence/living-in-ireland')
-})
+});
 
 router.get('/revenue.ie/online-services/file-an-income-tax-return', function (req, res) {
     res.render('revenue_ie/irish_tax_return')
 })
 // add your routes here
 
-module.exports = router
+module.exports = router;

9edf5c00c7dc1090848becb8215c08187482a321
Vanessa Formicola <vanessa@Vanessas-MacBook-Pro.local>
2017-12-13 16:11:04 +0000

    Tax balance with revenue page

--- app/routes.js
+++ app/routes.js
@@ -32,6 +32,8 @@ router.get('/revenue.ie/online-services/view_statement', function (req, res) {
 
 router.get('/revenue.ie/online-services/register_for_myaccount', function (req, res) {
   res.render('revenue_ie/register_for_myaccount')
+});
+
 router.get('/revenue.ie/online-services/tax-balance', function (req, res) {
   res.render('revenue_ie/tax_balance.html')
 });
@@ -42,8 +44,6 @@ router.get('/ros.ie/myaccount-register', function (req, res) {
 
 router.get('/ros.ie/myaccount-signin', function (req, res) {
   res.render('ros_ie/myaccount_signin')
-});
-
 });
 
 module.exports = router;
